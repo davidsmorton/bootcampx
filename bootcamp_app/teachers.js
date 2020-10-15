0e1f8b2330abc8c8f9d38aed4b6dd87d51b71f06
davidsmorton <davidsmorton@gmail.com>
2020-10-15 04:26:37 +0000

    updating files for sql injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,15 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+const cohortName = process.argv[2]
+const defaultCohortName = JUL02
+const values = [cohortName, defaultCohortName]
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] 
-  || 'JUL02'}'
+WHERE cohorts.name = '$1 
+  || $2'
 ORDER BY teacher;
 `)
 .then(res => {
