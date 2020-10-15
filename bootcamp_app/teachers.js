5b5e039502e5d5d4969c26836840b9bfc5643062
davidsmorton <davidsmorton@gmail.com>
2020-10-15 19:47:13 +0000

    refactoring and fixing connections to files and database

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 const cohortName = process.argv[2]
-const defaultCohortName = JUL02
-const values = [cohortName, defaultCohortName]
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const values = [cohortName ||'JUL02']
+const statment = 
+`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '$1 
-  || $2'
+WHERE cohorts.name = $1 
 ORDER BY teacher;
-`)
+`
+pool
+.query(statment, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
